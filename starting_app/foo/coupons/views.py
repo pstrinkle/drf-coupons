701103d0491a29dcd03c3882f68a5af0290726af
Patrick Trinkle <pstrink@gmail.com>
2017-01-08 19:37:14 -0800

    disable PATCH.

--- starting_app/foo/coupons/views.py
+++ starting_app/foo/coupons/views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from coupons.models import Coupon
 from coupons.serializers import CouponSerializer
@@ -11,3 +13,7 @@ class CouponViewSet(viewsets.ModelViewSet):
 
     serializer_class = CouponSerializer
     queryset = Coupon.objects.all()
+
+    def partial_update(self, request, pk=None, **kwargs):
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
