8fdbee3f510a05d4c464c70b15c171859fd87c9f
Patrick Trinkle <pstrink@gmail.com>
2017-01-15 19:26:32 -0800

    More tests

--- foo/foo/tests/test_item_coupon_create.py
+++ foo/foo/tests/test_item_coupon_create.py
@@ -31,3 +31,38 @@ class ItemCouponCreateTests(BasicTest):
         coupon['bound'] = False
 
         self.verify_built(coupon, response.data)
+
+    def test_can_redeem_coupon_within_item_app(self):
+        """
+        Redeem a coupon (boringly).
+        """
+
+        coupon = {
+            'code': 'ASDF',
+            'type': 'percent',
+        }
+
+        self.login(username='admin')
+        response = self.client.post('/api/v1/coupon', coupon, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        coupon['code_l'] = coupon['code'].lower()
+        coupon['repeat'] = 0
+        coupon['bound'] = False
+
+        self.verify_built(coupon, response.data)
+
+        coupon_id = response.data['id']
+
+        response = self.client.put('/api/v1/coupon/%s/redeem' % coupon_id, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.get('/api/v1/coupon/%s/redeemed' % coupon_id, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(1, len(response.data))
+
+        response = self.client.get('/api/v1/redeemed', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(1, len(response.data))
+
+        self.logout()
